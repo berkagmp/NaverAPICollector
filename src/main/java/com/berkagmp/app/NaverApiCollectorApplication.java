7bc28873d7f5837e95d892ad1d9f2a7b4979cbc0
Derek <berkagmp@gmail.com>
2020-06-14 13:37:39 +1200

    first commit

---
+++ src/main/java/com/berkagmp/app/NaverApiCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.berkagmp.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaverApiCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaverApiCollectorApplication.class, args);
+	}
+
+}
