c8805f9c9586d69421a673f81f774a4ae6f59a12
Derek <berkagmp@gmail.com>
2020-06-17 23:41:37 +1200

    Add method

--- src/main/java/com/berkagmp/app/repo/BrandRepository.java
+++ src/main/java/com/berkagmp/app/repo/BrandRepository.java
@@ -1,9 +1,11 @@
 package com.berkagmp.app.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.berkagmp.app.entity.Brand;
 
 public interface BrandRepository extends JpaRepository<Brand, Integer> {
-
+	public List<Brand> findByActive(Boolean active);
 }
