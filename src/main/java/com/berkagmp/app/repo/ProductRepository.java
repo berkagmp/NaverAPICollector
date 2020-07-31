343a62cb28c3c961e3fe355633caee830cff6df0
Derek <berkagmp@gmail.com>
2020-07-31 15:28:26 +1200

    Add method to Product Repo

--- src/main/java/com/berkagmp/app/repo/ProductRepository.java
+++ src/main/java/com/berkagmp/app/repo/ProductRepository.java
@@ -1,9 +1,9 @@
 package com.berkagmp.app.repo;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.berkagmp.app.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+  public List<Product> findByActive(Boolean active);
 }
